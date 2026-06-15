dbbaf206a44f680d2fcca4d1ac90221092a0f89a
Sathvik Reddy <sathvikureddy@gmail.com>
2026-06-16 00:10:08 +0530

    Add global requests timeout patch in gateway config

--- gateway/config.py
+++ gateway/config.py
@@ -1,4 +1,13 @@
 import os
+import requests
+
+# Global patch to prevent requests from blocking indefinitely
+original_request = requests.Session.request
+def patched_request(self, method, url, **kwargs):
+    if 'timeout' not in kwargs:
+        kwargs['timeout'] = 10.0
+    return original_request(self, method, url, **kwargs)
+requests.Session.request = patched_request
 
 SPRING_BOOT_URL = os.environ.get("SPRING_BOOT_URL") or os.environ.get("SPRING_BACKEND_URL") or "http://localhost:8080"
 NODE_JS_URL = os.environ.get("NODE_JS_URL") or "http://localhost:5001"
