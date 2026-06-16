d7972254a9940125003fe907e9fff0975befcc93
Sathvik Reddy <sathvikureddy@gmail.com>
2026-06-16 09:56:20 +0530

    fix: resolve Render name resolution error using dynamic fromService mapping for backend and nodejs

--- gateway/config.py
+++ gateway/config.py
@@ -9,5 +9,18 @@ def patched_request(self, method, url, **kwargs):
     return original_request(self, method, url, **kwargs)
 requests.Session.request = patched_request
 
-SPRING_BOOT_URL = os.environ.get("SPRING_BOOT_URL") or os.environ.get("SPRING_BACKEND_URL") or "http://localhost:8080"
-NODE_JS_URL = os.environ.get("NODE_JS_URL") or "http://localhost:5001"
+SPRING_BOOT_HOST = os.environ.get("SPRING_BOOT_HOST")
+SPRING_BOOT_PORT = os.environ.get("SPRING_BOOT_PORT")
+
+if SPRING_BOOT_HOST:
+    SPRING_BOOT_URL = f"http://{SPRING_BOOT_HOST}:{SPRING_BOOT_PORT or '8080'}"
+else:
+    SPRING_BOOT_URL = os.environ.get("SPRING_BOOT_URL") or os.environ.get("SPRING_BACKEND_URL") or "http://localhost:8080"
+
+NODE_JS_HOST = os.environ.get("NODE_JS_HOST")
+NODE_JS_PORT = os.environ.get("NODE_JS_PORT")
+
+if NODE_JS_HOST:
+    NODE_JS_URL = f"http://{NODE_JS_HOST}:{NODE_JS_PORT or '5001'}"
+else:
+    NODE_JS_URL = os.environ.get("NODE_JS_URL") or "http://localhost:5001"
